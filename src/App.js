815d2987c4356092590186d24e915a518b292060
gprok <gprok60@gmail.com>
2021-06-20 13:45:47 +0300

    get, save, and delete joke

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import ChuckProvider from './context/ChuckProvider'
+import Joke from './components/Joke'
+import SavedJokes from './components/SavedJokes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChuckProvider>
+      <div>
+        <header>
+          <h1>
+            Chuck Norris Jokes App
+          </h1>
+        </header>
+        <Joke />
+        <SavedJokes />
+      </div>
+    </ChuckProvider>
   );
 }
 
